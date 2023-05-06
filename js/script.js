535fa420ba7b4ac316f7dfdbdfd613bc601b9c83
Chelsea Griffo <chelseagriffo@gmail.com>
2023-05-05 23:56:25 -0500

    Continued creating JS objects

--- js/script.js
+++ js/script.js
@@ -1,30 +1,77 @@
-/******************************************
-Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
-******************************************/
+ let quotes = [
+   {quote: '"I don\'t want to talk to you no more, you empty-headed animal food trough wiper. I fart in your general direction. Your mother was a hamster and your father smelt of elderberries."', 
+   citation: '\n- French Knight', 
+   source: '<i>Monty Python and the Holy Grail</i>', 
+   year: '1975'}, 
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+   {quote: '"Having a luck dragon with you is the only way to go on a quest."',
+   citation: '\n- Falkor', 
+   source: '<i>The Neverending story</i>', 
+   year: '1984'}, 
 
+   {quote: '"You wouldn\'t be so brave if you\'d ever smelled the Bog of Eternal Stench."', 
+   citation: '\n- Hoggle', 
+   source: '<i>The Labrynth</i>', 
+   year: '1986'}, 
 
- let quotes = [{
-   "I don’t want to talk to you no more, you empty-headed animal food trough wiper. I fart in your general direction. Your mother was a hamster and your father smelt of elderberries.": '\n– French Knight, <i>Monty Python and the Holy Grail</i>, 1975', 
-   "Having a luck dragon with you is the only way to go on a quest.": '\n- Falkor, <i>The Neverending story</i>, 1984', 
-   "You wouldn't be so brave if you'd ever smelled the Bog of Eternal Stench.": '\n- Hoggle, <i>The Labrynth</i>, 1986', 
-   "If you can't say anything nice about anybody, come sit by me.": '\n- Clairee, <i>Steel Magnolias</i>, 1989', 
-   "Well, my stupid, sorry, parasitic sacks of entrails... revenge is mine.": '\n- Captain James Hook, <i>Hook</i>, 1991', 
-   "Pride is an abomination. One must forego the self to obtain total spiritual creaminess, and avoid the chewy chunks of degradation.": '\n- Ace Ventura, <i>Ace Venture When Nature Calls</i>, 1995',
-   "Wasn't your... didn't your mole used to be on the other side?": '\n- Sheriff of Rottingham, <i>Robin Hood: Men in Tights<\i>, 1993', 
-   "Into the pit with those bloody-thirsty sons of whores!": '\n- Old Woman, <i>Army Of Darkness</i>, 1992',
-   "What was sundered and undone, shall be whole, the two made one!": '\n- Mother Aughra, <i>The Dark Crystal</i>, 1982',
-   "So. At Last, We Meet For The First Time, For The Last Time.": '\n- Dark Helmet, <i>Space Balls</i>, 1987',
-   "I can't sleep anymore. It's too much like death.": '\n- Cosmo Castorini, <i>Moonstruck</i>, 1987',
-   "Someday we'll find it, the rainbow connection. The lovers, the dreamers, and me.": '\n- Kermit the Frog, <i>The Muppet Movie</i>, 1979',
-   "You mean you'll put down your rock and I'll put down my sword, and we'll try and kill each other like civilized people?": '\n- Westley, <i>The Princess Bride</i>, 1987',
-   "You’re such a sweet boy, letting an old woman pay you with meatballs!": '\n- Rosie, <i>The Wedding Singer</i>, 1998',
-   "By Grabthar's hammer, by the suns of Worvan, you shall be avenged.": '\n- Sir Alexander Dane, <i>Galaxy Quest</i>, 1999'
- }];
+   {quote: '"If you can\'t say anything nice about anybody, come sit by me."', 
+   citation: '\n- Clairee', 
+   source: '<i>Steel Magnolias</i>', 
+   year: '1989'}, 
+
+   {quote: '"Well, my stupid, sorry, parasitic sacks of entrails... revenge is mine."', 
+   citation: '\n- Captain James Hook', 
+   source: '<i>Hook</i>', 
+   year: '1991'}, 
+
+   {quote: '"Pride is an abomination. One must forego the self to obtain total spiritual creaminess, and avoid the chewy chunks of degradation."', 
+   citation: '\n- Ace Ventura', 
+   source: '<i>Ace Venture When Nature Calls</i>', 
+   year: '1995'},
+
+   {quote: '"Wasn\'t your... didn\'t your mole used to be on the other side?"', 
+   citation: '\n- Sheriff of Rottingham', 
+   source: '<i>Robin Hood: Men in Tights<\i>', 
+   year: '1993'}, 
+
+   {quote: '"Into the pit with those bloody-thirsty sons of whores!"', 
+   citation: '\n- Old Woman', 
+   source: '<i>Army Of Darkness</i>', 
+   year: '1992'},
+
+   {quote: '"What was sundered and undone, shall be whole, the two made one!"', 
+   citation: '\n- Mother Aughra', 
+   source: '<i>The Dark Crystal</i>', 
+   year: '1982'},
+
+   {quote: '"So. At Last, We Meet For The First Time, For The Last Time."', 
+   citation: '\n- Dark Helmet', 
+   source: '<i>Space Balls</i>', 
+   year: '1987'},
+
+   {quote: '"I can\'t sleep anymore. It\'s too much like death."', 
+   citation: '\n- Cosmo Castorini', 
+   source: '<i>Moonstruck</i>', 
+   year: '1987'},
+
+   {quote: '"Someday we\'ll find it, the rainbow connection. The lovers, the dreamers, and me."', 
+   citation: '\n- Kermit the Frog', 
+   source: '<i>The Muppet Movie</i>', 
+   year: '1979'},
+   {quote: '"You mean you\'ll put down your rock and I\'ll put down my sword, and we\'ll try and kill each other like civilized people?"', 
+   citation: '\n- Westley', 
+   source: '<i>The Princess Bride</i>', 
+   year: '1987'},
+   {quote: '"You’re such a sweet boy, letting an old woman pay you with meatballs!"', 
+   citation: '\n- Rosie', 
+   source: '<i>The Wedding Singer</i>', 
+   year: '1998'},
+
+   {quote: '"By Grabthar\'s hammer, by the suns of Worvan, you shall be avenged."', 
+   citation: '\n- Sir Alexander Dane', 
+   source: '<i>Galaxy Quest</i>', 
+   year: '1999'}
+ ];
 
 
 
