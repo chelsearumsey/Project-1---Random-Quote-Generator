f079b64c1cb3bbc43aea20c3858f1dc7304aab0c
Chelsea Griffo <chelseagriffo@gmail.com>
2023-05-05 23:29:45 -0500

    Created movie quotes array

--- js/script.js
+++ js/script.js
@@ -7,9 +7,24 @@ project 1 - A Random Quote Generator
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
-***/
+
+ let quotes = [
+   "I don’t want to talk to you no more, you empty-headed animal food trough wiper. I fart in your general direction. Your mother was a hamster and your father smelt of elderberries." '\n– French Knight, <i>Monty Python and the Holy Grail</i>, 1975', 
+   "Having a luck dragon with you is the only way to go on a quest." '\n- Falkor, <i>The Neverending story</i>, 1984', 
+   "You wouldn't be so brave if you'd ever smelled the Bog of Eternal Stench." '\n- Hoggle, <i>The Labrynth</i>, 1986', 
+   "If you can't say anything nice about anybody, come sit by me." '\n- Clairee, <i>Steel Magnolias</i>, 1989', 
+   "Well, my stupid, sorry, parasitic sacks of entrails... revenge is mine." '\n- Captain James Hook, <i>Hook</i>, 1991', 
+   "Pride is an abomination. One must forego the self to obtain total spiritual creaminess, and avoid the chewy chunks of degradation."'\n- Ace Ventura, <i>Ace Venture When Nature Calls</i>, 1995',
+   "Wasn't your... didn't your mole used to be on the other side?"'\n- Sheriff of Rottingham, <i>Robin Hood: Men in Tights<\i>, 1993', 
+   "Into the pit with those bloody-thirsty sons of whores!" '\n- Old Woman, <i>Army Of Darkness</i>, 1992',
+   "What was sundered and undone, shall be whole, the two made one!"'\n- Mother Aughra, <i>The Dark Crystal</i>, 1982',
+   "So. At Last, We Meet For The First Time, For The Last Time."'\n- Dark Helmet, <i>Space Balls</i>, 1987',
+   "I can't sleep anymore. It's too much like death."'\n- Cosmo Castorini, <i>Moonstruck</i>, 1987',
+   "Someday we'll find it, the rainbow connection. The lovers, the dreamers, and me."'\n- Kermit the Frog, <i>The Muppet Movie</i>, 1979',
+   "You mean you'll put down your rock and I'll put down my sword, and we'll try and kill each other like civilized people?" '\n- Westley, <i>The Princess Bride</i>, 1987',
+   "You’re such a sweet boy, letting an old woman pay you with meatballs!"'\n- Rosie, <i>The Wedding Singer</i>, 1998',
+   "By Grabthar's hammer, by the suns of Worvan, you shall be avenged."'\n- Sir Alexander Dane, <i>Galaxy Quest</i>, 1999'
+ ];
 
 
 
